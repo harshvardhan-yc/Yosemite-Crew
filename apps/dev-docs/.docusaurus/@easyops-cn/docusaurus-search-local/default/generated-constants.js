830929428de3dac041ebede831bb0aaabf74764b
harshit-yc <harshit@yosemitecrew.com>
2025-11-19 12:20:02 +0530

    git ignore docusorus

--- apps/dev-docs/.docusaurus/@easyops-cn/docusaurus-search-local/default/generated-constants.js
+++
@@ -1,6 +0,0 @@
-import lunr from "/Users/harshitwandhare/Desktop/Yosemite-Crew/node_modules/.pnpm/lunr@2.3.9/node_modules/lunr/lunr.js";
-export const removeDefaultStopWordFilter = [];
-export const language = ["en"];
-export const searchIndexUrl = "search-index{dir}.json?_=830cffc3";
-export const searchResultLimits = 8;
-export const fuzzyMatchingDistance = 1;
--- apps/dev-docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/harshitwandhare/Desktop/Yosemite-Crew/node_modules/.pnpm/infima@0.2.0-alpha.45/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/harshitwandhare/Desktop/Yosemite-Crew/node_modules/.pnpm/@docusaurus+theme-classic@3.9.2_@types+react@19.1.12_react-dom@18.3.1_react@18.3.1_typescript@5.6.3/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/harshitwandhare/Desktop/Yosemite-Crew/node_modules/.pnpm/@docusaurus+theme-classic@3.9.2_@types+react@19.1.12_react-dom@18.3.1_react@18.3.1_typescript@5.6.3/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/harshitwandhare/Desktop/Yosemite-Crew/apps/dev-docs/src/css/custom.css"),
-];
