434714aec436db5cd58af39a832f13e0db3fdabc
harshvardhan-yc <harshvardhan@yosemitecrew.com>
2026-06-05 21:12:35 +0530

    feat(fhir): adds FHIR generator script for release versions
    Signed-off-by: harshvardhan-yc <harshvardhan@yosemitecrew.com>

--- packages/fhir/src/index.ts
+++ packages/fhir/src/index.ts
@@ -1,1 +1,26 @@
-export {};
+export * from './r4/index.js';
+export * as R4 from './r4/index.js';
+
+import type { Money } from './r4/index.js';
+
+export type Currency = NonNullable<Money['currency']>;
+
+export type QuestionnaireStatus = 'draft' | 'active' | 'retired' | 'unknown';
+
+export type QuestionnaireItemType =
+  | 'group'
+  | 'display'
+  | 'boolean'
+  | 'decimal'
+  | 'integer'
+  | 'date'
+  | 'dateTime'
+  | 'time'
+  | 'string'
+  | 'text'
+  | 'url'
+  | 'choice'
+  | 'open-choice'
+  | 'attachment'
+  | 'reference'
+  | 'quantity';
